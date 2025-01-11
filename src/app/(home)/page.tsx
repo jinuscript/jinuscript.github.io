1f4de97e585205c1a1bc436410940891c382b47a
rlawlsdn263 <rlawlsdn263@naver.com>
2025-01-11 18:16:32 +0900

    FEAT: HomePage에서 Post 리스트 렌더링

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,15 @@
+import { fetchAllIssues } from "@/lib";
+import { Post } from "@/app/(home)/_ui";
 import styles from "./HomePage.module.css";
 
-export default function HomePage() {
-  return <div className={styles.HomePage}>홈페이지</div>;
+export default async function HomePage() {
+  const issues = await fetchAllIssues();
+
+  return (
+    <main className={styles.HomePage}>
+      {issues.map((issue) => (
+        <Post key={issue.id} post={issue} />
+      ))}
+    </main>
+  );
 }
